c3ea309759332ab512e98e1f018503f9283e5254
ayushXdev <ayush.bagg88@gmail.com>
2025-02-13 14:52:37 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your <br /> favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas recusandae nesciunt obcaecati, saepe aliquam, rem dolorum praesentium, quas suscipit eaque non mollitia odio. Illo, ratione. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor aliquam pariatur.</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
